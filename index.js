7f555b7cf8be68dee112f75f9ca6787f6e2f640e
Zion Yang <zionremnant@gmail.com>
2022-03-24 01:51:25 -0700

    manager init index.js

--- index.js
+++ index.js
@@ -15,6 +15,46 @@ const Employee = {
   Manager: "Manager",
 };
 
-function init() {}
+function init() {
+  function managerCreate() {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the manager's name?",
+        validate: (response) => {
+          return validation.required(response);
+        },
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the manager's employee Id?",
+        validate: (response) => {
+          return validation.required(response);
+        },
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the manager's email address?",
+        validate: (response) => {
+          return validation.required(response) && validation.email(response);
+        },
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is the manager's office number?",
+        validate: (response) => {
+          return validation.required(response);
+        },
+      },
+    ]);
+    // .then[({name, id, email, officeNumber]} => {
+    //   const manager = new Manager(name, id, email, officeNumber)
+    // })
+  }
+}
 
 init();
