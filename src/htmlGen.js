02d101d33f98c40f073ef3d92b22d63c58d879b4
Zion Yang <zionremnant@gmail.com>
2022-03-24 22:14:43 -0700

    getUsername Engineer.js

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -15,3 +15,20 @@ const generateManagerCard = (manager) => {
 </div>
 `;
 };
+const generateEngineerCard = (engineer) => {
+  return `
+    <div class="card">
+    <h3>${engineer.getName()}</h3>
+    <p>${engineer.getRole()}</p>
+    <div class="card-body">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineer.getID()}</li>
+        <li class="list-group-item">
+          Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
+        </li>
+        <li class="list-group-item">Office: ${engineer.getUsername()}</li>
+      </ul>
+    </div>
+  </div>
+  `;
+};
